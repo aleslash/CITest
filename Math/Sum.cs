749f7dce1df2863e28d6e9f7731c99f4d988e6e9
aleslash <aleslash@gmail.com>
2012-08-07 21:50:53 -0300

    Inicio

---
+++ Math/Sum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Math
+{
+    public class Sum
+    {
+        public static long AddTwoNumbers(long number1, long number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
