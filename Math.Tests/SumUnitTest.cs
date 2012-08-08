fde6f7089d49922e460f966f0887b26fb7dca23b
aleslash <aleslash@gmail.com>
2012-08-07 22:47:01 -0300

    Tests added

---
+++ Math.Tests/SumUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Math;
+
+namespace Math.Tests
+{
+    [TestClass]
+    public class SumUnitTest
+    {
+        [TestMethod]
+        public void OKAddTwoNumbers()
+        {
+            Assert.AreEqual(Sum.AddTwoNumbers(10, 11), 21); 
+        }
+    }
+}
